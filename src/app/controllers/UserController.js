f1dea540bb763a8eb20257bfb9870b8c64512de8
rodriguesporan <rodriguesporan@gmail.com>
2019-06-27 14:44:40 -0300

    Restringido o acesso à rotas que não contenham o token no header

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -9,6 +9,10 @@ class UserController {
     const { id, name, email } = await User.create(req.body);
     return res.json({ id, name, email });
   }
+
+  async update(req, res) {
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
