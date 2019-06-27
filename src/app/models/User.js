c9f1f5960c11c80e884d1977b68d9c1a3346a6e4
rodriguesporan <rodriguesporan@gmail.com>
2019-06-27 13:09:08 -0300

    Gerando has de senha

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -7,9 +8,18 @@ class User extends Model {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
         password_hash: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
       },
       { sequelize }
     );
+
+    this.addHook('beforeSave', async user => {
+      if (!user.password_hash) {
+        user.password = await bcrypt.hash(user.password_hash, 8);
+      }
+    });
+
+    return this;
   }
 }
 
